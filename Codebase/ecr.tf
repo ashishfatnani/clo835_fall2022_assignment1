dc34347ef1d32284ac552408204859dd9e49f712
Ashish Fatnani <ashishfatnani3@gmail.com>
2023-02-06 21:20:41 +0000

    feat:changes in developement (done)

---
+++ Codebase/ecr.tf
@@ -0,0 +1,6 @@
+# resource "aws_ecr_repository" "application_ecr_repo" {
+#   name = "application_ecr_repo"
+# }
+# resource "aws_ecr_repository" "database_ecr_repo" {
+#   name = "database_ecr_repo"
+# }
---
+++ Codebase/variables.tf
@@ -0,0 +1,30 @@
+
+# variable "region" {
+#   default     = "us-east-1"
+#   type        = string
+#   description = "AWS Region"
+# }
+
+# variable "amiId" {
+#   default     = "ami-0aa7d40eeae50c9a9"
+#   type        = string
+#   description = "AMI ID"
+# }
+
+# variable "ec2Name" {
+#   default     = "webapp_server"
+#   type        = string
+#   description = "EC2 instance name"
+# }
+
+# variable "cidrBlock" {
+#   default     = "172.31.96.0/20"
+#   type        = string
+#   description = "CIDR Block"
+# }
+
+# variable "securityGroupName" {
+#   default     = "webapp_sg_assign1"
+#   type        = string
+#   description = "Security group for webapp server"
+# }
